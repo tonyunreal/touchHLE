43ebf63327ff3dc37681c4a1f00094d5e74fd91c
tonyunreal <tonyunreal@users.noreply.github.com>
2026-03-27 22:29:49 +0800

    added AudioServicesCreateSystemSoundID

--- src/frameworks/audio_toolbox/audio_services.rs
+++ src/frameworks/audio_toolbox/audio_services.rs
@@ -6,6 +6,7 @@
 //! `AudioServices.h` (Audio Services)
 
 use crate::dyld::{export_c_func, FunctionExports};
+use crate::frameworks::core_foundation::cf_url::CFURLRef;
 use crate::frameworks::carbon_core::OSStatus;
 use crate::frameworks::core_audio_types::fourcc;
 use crate::mem::{MutPtr, MutVoidPtr};
@@ -41,7 +42,14 @@ fn AudioServicesPlaySystemSound(_env: &mut Environment, in_system_sound_id: Syst
     // TODO: implement other system sounds
 }
 
+fn AudioServicesCreateSystemSoundID(_env: &mut Environment, inFileURL: CFURLRef, outSystemSoundID: MutPtr<SystemSoundID>) -> OSStatus {
+    // TODO: implement other system sounds
+    _env.mem.write(outSystemSoundID, kSystemSoundID_Vibrate);
+    0
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(AudioServicesGetProperty(_, _, _, _, _)),
     export_c_func!(AudioServicesPlaySystemSound(_)),
+    export_c_func!(AudioServicesCreateSystemSoundID(_, _)),
 ];
